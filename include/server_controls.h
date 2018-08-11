df984bb15b3c477c8ac5d0fa86ba468af539b7b3
Ilia Yakovlev <airenilia@gmail.com>
2018-08-11 18:49:22 +0700

    Event structure modified, added event set and get functions

--- include/server_controls.h
+++ include/server_controls.h
@@ -7,13 +7,13 @@
 #ifndef SERVER_CONTROLS_H
 #define SERVER_CONTROLS_H
 
-///Event id when player eaten by another.\warning Need arg of type struct xy_t when used! x - player eaten id and y - player ate id.
+///Event id when player eaten by another.\warning Need both args when used in event_t! x - player eaten id and y - player ate id.
 #define EVENT_PLAYER_EATEN 1
-///Event id when player ate a pellet.\warning Need arg of type struct xy_t when used! x - pellet eaten id and y - pellet ate id.
+///Event id when player ate a pellet.\warning Need both args when used in event_t! x - pellet eaten id and y - pellet ate id.
 #define EVENT_PELLET_EATEN 2
-///Event id when player change size.\warning Need arg of type struct xy_t when used! x - player id and y - size.\todo Shoot yourself into players.
+///Event id when player change size.\warning Need both args when used in event_t! x - player id and y - size.\todo Shoot yourself into players.
 #define EVENT_PLAYER_SIZE 3
-///Event id when player moves.\warning Need arg of type int when used!
+///Event id when player moves.\warning Need only argument X when used! 1 - up, 2 - down, 3 - left, 4 - right and 0 - if all buttons released.
 #define EVENT_PLAYER_MOVE 4
 
 ///Struct for two values type of int.\note Used for coordinates, speed, or as argument for event.
@@ -26,9 +26,9 @@ typedef struct xy {
 typedef struct game_parameters {
 	int def_player_size; ///<	Default player size.
 	int def_pellet_size; ///<	Default pellet size.
-	float def_speed; ///<		Default maximum player speed.
-	float def_accel; ///<		Default acceleration.
-	float def_drag; ///<		Default drag force.
+	int def_speed; ///<		Default maximum player speed.
+	int def_accel; ///<		Default acceleration.
+	int def_drag; ///<		Default drag force.
 	useconds_t player_pace; ///<	Time in usec between every move cycle.
 	useconds_t pellet_pace; ///<	Time in usec between every pellet generation.
 } game_parameters_t;
@@ -39,7 +39,8 @@ typedef struct player {
 	int size; ///<		Player size.
 	xy_t position; ///<	Player position on the field.
 	xy_t speed; ///<	Player speed in X, Y direction.
-	float drag; ///<	How much force slows player down.
+	xy_t accel; ///<	Player acceleration in X, Y direction.
+	int drag; ///<		How much force slows player down.
 } player_t;
 
 ///Struct containing player parameters.
@@ -61,7 +62,9 @@ typedef struct gamefield {
 ///Game event struct.
 typedef struct event {
 	int event_id; ///<	Predefined id of event.
-	void *arg; ///<		Argument of event.
+	int arg_x; ///<		Argument X of event.
+	int arg_y; ///<		Argument Y of event.
+	struct event *next; ///<Pointer to next event (NULL by default).
 } event_t;
 
 /**Create game field.
@@ -79,15 +82,28 @@ int gamefield_add(gamefield_t*);
 
 /**Begin server calculations, collision detections etc.
 \param gamefield_t*	Gamefield struct pointer.
-\param event_t* 	Array of output events pointer.\todo Maybe implement socket. But i hope there will be ony one elment.
-\param event_t* 	Array of input events pointer.\todo Maybe implement socket. But i hope there will be ony one elment.
+\param event_t** 	List of output events pointer.\todo Maybe implement socket. But i hope there will be ony one elment.
+\param event_t** 	List of input events pointer.\todo Maybe implement socket. But i hope there will be ony one elment.
 \return int		Return 0 if success, else -1.	
 */
-int gamefield_start(gamefield_t*, event_t*, event_t*);
+int gamefield_start(gamefield_t*, event_t**, event_t**);
 
 /**Cancel server controls threads, free gamefield structures.\warning Won't free events arrays.
 \param gamefield_t*	Gamefield struct pointer.
 \return int		Return 0 if success, else spinning emmensly begging for death.
 */
 int gamefield_free(gamefield_t* gamefield);
+
+/**Insert event into event list.
+\param event_list	Address of (pointer to) event list pointer.\warning Pointer must be NULL before use. For example: event_t *event_in = NULL; event_set(&event_in, event);
+\param event		Event to insert.
+\return int		Return 0 if success, else -1.
+*/
+int event_set(event_t** event_list, event_t event);
+
+/**Get event from event list.
+\param event_list	Address of (pointer to) event list pointer.
+\return event_t		Return last event from list, or event_t with event_id = 0 if no events found.
+*/
+event_t event_get(event_t** event_list);
 #endif
--- src/server_controls.c
+++ src/server_controls.c
@@ -1,8 +1,12 @@
 #include "../include/server_controls.h"
 
-#define SERVER_CONTROLS_THREADS_COUNT 3
+#define SERVER_CONTROLS_THREADS_COUNT 4
 game_parameters_t game_parameters;
 pthread_t *tid;
+event_t **event_glob_in;
+event_t **event_glob_out;
+//pthread_spinlock_t spinlock;
+pthread_mutex_t spinlock;///<\todo EBANYE SPINLOKI
 
 int gamefield_free(gamefield_t* gamefield)
 {
@@ -16,17 +20,21 @@ int gamefield_free(gamefield_t* gamefield)
 	free(gamefield->players);
 	free(gamefield->pellets);
 	free(gamefield);
+	//pthread_spin_destroy(&spinlock);
+	pthread_mutex_destroy(&spinlock);
 	return 0;
 }
 
 gamefield_t* gamefield_create(int width, int height)
 {
 	srand(time(NULL));
+	//pthread_spin_init(&spinlock, PTHREAD_PROCESS_SHARED);
+	pthread_mutex_init(&spinlock, NULL);
 	game_parameters.def_player_size = 10;
 	game_parameters.def_pellet_size = 5;
-	game_parameters.def_speed = 1.0;
-	game_parameters.def_accel = 0.4;
-	game_parameters.def_drag = 0.2;
+	game_parameters.def_speed = 100;
+	game_parameters.def_accel = 10;
+	game_parameters.def_drag = 1;
 	game_parameters.player_pace = 200000;
 	game_parameters.pellet_pace = 1000000;
 
@@ -52,6 +60,8 @@ int gamefield_add(gamefield_t* gamefield)
 	player.position.y = rand()%gamefield->size.y;
 	player.speed.x = 0;
 	player.speed.y = 0;
+	player.accel.x = 0;
+	player.accel.y = 0;
 	player.drag = 0;
 	gamefield->players[player_id] = player;
 	gamefield->players_count++;
@@ -71,7 +81,7 @@ void *cycle_make_pellet(void* v_gamefield)
 		pellet.position.y = rand()%gamefield->size.y;
 		gamefield->pellets[gamefield->pellets_count] = pellet;
 		gamefield->pellets_count++;
-		usleep(game_parameters.pellet_pace);
+		usleep(game_parameters.pellet_pace); ///\todo Global timer.
 		pthread_testcancel();
 	}
 }
@@ -84,42 +94,171 @@ void *cycle_move(void* v_gamefield)
 	{
 		for(i = 0; i < gamefield->players_count; i++)
 		{
+			//Acceleration
+			if(gamefield->players[i].speed.x < game_parameters.def_speed)
+				gamefield->players[i].speed.x += gamefield->players[i].accel.x;
+			else
+				gamefield->players[i].speed.x = game_parameters.def_speed;
+			if(gamefield->players[i].speed.y < game_parameters.def_speed)
+				gamefield->players[i].speed.y += gamefield->players[i].accel.y;
+			else
+				gamefield->players[i].speed.y = game_parameters.def_speed;
+			//Drag
+			if(gamefield->players[i].speed.x > 0)
+				gamefield->players[i].speed.x -= gamefield->players[i].drag;
+			else
+				gamefield->players[i].speed.x = 0;
+			if(gamefield->players[i].speed.y > 0)
+				gamefield->players[i].speed.y -= gamefield->players[i].drag;
+			else
+				gamefield->players[i].speed.y = 0;
+			//Move
 			gamefield->players[i].position.x += gamefield->players[i].speed.x;
 			gamefield->players[i].position.y += gamefield->players[i].speed.y;
 		}
-		usleep(game_parameters.player_pace);
+		usleep(game_parameters.player_pace); ///\todo Global timer.
 		pthread_testcancel();
 	}
 }
 
-struct gamefield_and_events {
-	gamefield_t *gamefield;
-	event_t *event_out;
-	event_t *event_in;
-};
+//struct gamefield_and_events {
+//	gamefield_t *gamefield;
+//	event_t ***event;
+//};
 
-void *cycle_controls(void* v_gamefield_and_events)
+//void *cycle_controls_in(void* v_gamefield_and_events)
+void *cycle_controls_in(void* v_gamefield)///<\todo PEREDAT YKAZATEL NA *EVENT V PARAMETER
 {
-	struct gamefield_and_events *gamefield_and_events = v_gamefield_and_events;
-	gamefield_t *gamefield = gamefield_and_events->gamefield;
-	event_t* event_out = gamefield_and_events->event_out;
-	event_t* event_in = gamefield_and_events->event_in;
+	//struct gamefield_and_events *gamefield_and_events = v_gamefield_and_events;
+	//gamefield_t *gamefield = gamefield_and_events->gamefield;
+	//event_t **event_in = *gamefield_and_events->event;
+	gamefield_t *gamefield = v_gamefield;
+	event_t event;
+	while(1)
+	{
+		//event = event_get(event_in);
+		event = event_get(event_glob_in);
+		//if(event.event_id != -1)
+		//	printf("!!!!!!!!!!!!!!!!!GOT!!!!!!!!!!!!!!!!!!!!!!!!!!!!%d\n", event.event_id);
+		pthread_testcancel();
+	}
 }
 
-int gamefield_start(gamefield_t* gamefield, event_t* event_out, event_t* event_in)
+//void *cycle_controls_out(void* v_gamefield_and_events)
+void *cycle_controls_out(void* v_gamefield)///<\todo PEREDAT YKAZATEL NA *EVENT V PARAMETER
 {
+	//struct gamefield_and_events *gamefield_and_events = v_gamefield_and_events;
+	//gamefield_t *gamefield = gamefield_and_events->gamefield;
+	//event_t **event_out = *gamefield_and_events->event;
+	gamefield_t *gamefield = v_gamefield;
+	//int i=0;
+	//event_t event;
+	while(1)
+	{
+		//i++;
+		//event.event_id = i;
+		//event.arg_x=i;
+		//event.arg_y =i;
+		//event.next=NULL;
+		//if(event_set(event_out, event) < 0)
+	 	//if(event_set(event_glob_out, event) < 0)
+		//	perror("Can't set event");
+		//sleep(1);
+		pthread_testcancel();
+	}
+}
+
+int gamefield_start(gamefield_t* gamefield, event_t** event_out, event_t** event_in)
+{
+	//struct gamefield_and_events gamefield_and_events_in, gamefield_and_events_out;
 	tid = malloc(SERVER_CONTROLS_THREADS_COUNT * sizeof(pthread_t));
+	event_glob_out = event_out;
+	event_glob_in = event_in;
 	if(tid == NULL)
 		return -1;
+
 	if(pthread_create(&tid[0], NULL, cycle_make_pellet, gamefield) > 0)
 		return -1;
+
 	if(pthread_create(&tid[1], NULL, cycle_move, gamefield) > 0)
 		return -1;
-	struct gamefield_and_events gamefield_and_events;
-	gamefield_and_events.gamefield = gamefield;
-	gamefield_and_events.event_out = event_out;
-	gamefield_and_events.event_in = event_in;
-	if(pthread_create(&tid[2], NULL, cycle_controls, &gamefield_and_events) > 0)
+
+	//gamefield_and_events_in.gamefield = gamefield;
+	//gamefield_and_events_in.event = &event_in;
+	//if(pthread_create(&tid[2], NULL, cycle_controls_in, &gamefield_and_events_in) > 0)
+	if(pthread_create(&tid[2], NULL, cycle_controls_in, gamefield) > 0)
+		return -1;
+	//gamefield_and_events_out.gamefield = gamefield;
+	//gamefield_and_events_out.event = &event_out;
+	//if(pthread_create(&tid[3], NULL, cycle_controls_out, &gamefield_and_events_out) > 0)
+	if(pthread_create(&tid[3], NULL, cycle_controls_out, gamefield) > 0)
 		return -1;
 	return 0;
+}
+
+int event_set(event_t **event_list, event_t event)
+{
+	//pthread_spin_lock(&spinlock);
+	pthread_mutex_lock(&spinlock);
+	event_t *new_event = malloc(sizeof(event_t));
+	if(new_event == NULL)
+		return -1;
+	*new_event = event;
+	if(*event_list == NULL) {
+		*event_list = new_event;
+		//pthread_spin_unlock(&spinlock);
+		pthread_mutex_unlock(&spinlock);
+		return 0;
+	}
+	else {
+		event_t *event_ptr = *event_list;
+		while(event_ptr->next != NULL)
+			event_ptr = event_ptr->next;
+		event_ptr->next = new_event;
+		//pthread_spin_unlock(&spinlock);
+		pthread_mutex_unlock(&spinlock);
+		return 0;
+	}
+	//pthread_spin_unlock(&spinlock);
+		pthread_mutex_unlock(&spinlock);
+	return -1;
+}
+
+event_t event_get(event_t **event_list)
+{
+	event_t got_event;
+	got_event.event_id = -1;
+	//pthread_spin_lock(&spinlock);
+	pthread_mutex_lock(&spinlock);
+	if(*event_list == NULL) {
+		//pthread_spin_unlock(&spinlock);
+		pthread_mutex_unlock(&spinlock);
+		return got_event;
+	}
+
+	if((*event_list)->next == NULL) {
+		got_event = **event_list;
+		free(*event_list);
+		*event_list = NULL;
+		//pthread_spin_unlock(&spinlock);
+		pthread_mutex_unlock(&spinlock);
+		return got_event;
+	}
+	else {
+		event_t *prev_event_ptr;
+		event_t *event_ptr = *event_list;
+		while(event_ptr->next != NULL) {
+			prev_event_ptr = event_ptr;
+			event_ptr = event_ptr->next;
+		}
+		got_event = *event_ptr;
+		free(event_ptr);
+		prev_event_ptr->next = NULL;
+		//pthread_spin_unlock(&spinlock);
+		pthread_mutex_unlock(&spinlock);
+		return got_event;
+	}
+	//pthread_spin_unlock(&spinlock);
+		pthread_mutex_unlock(&spinlock);
+	return got_event;
 }
